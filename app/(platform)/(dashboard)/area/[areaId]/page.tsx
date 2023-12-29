be4e78f4ab0e6e1c53a729b9aea8752b88c75753
OnjoujiToki <z@moon.moe>
2023-12-28 21:39:59 -0500

    restructure

---
+++ app/(platform)/(dashboard)/area/[areaId]/page.tsx
@@ -0,0 +1,16 @@
+import { Suspense } from "react";
+
+import { Separator } from "@/components/ui/separator";
+
+
+const OrganizationIdPage = async () => {
+
+
+  return (
+    <div className="w-full mb-20">
+
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
